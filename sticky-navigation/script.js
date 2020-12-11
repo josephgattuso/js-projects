2cd8b5d1ca6c76bacad72de77e324338c9680070
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-12-11 15:45:47 +0000

    add nav animation

--- sticky-navigation/script.js
+++ sticky-navigation/script.js
@@ -2,7 +2,6 @@ const nav = document.querySelector('.nav');
 window.addEventListener('scroll', fixNav);
 
 function fixNav() {
-  // console.log(window.scrollY, nav.offsetHeight);
   if (window.scrollY > nav.offsetHeight + 150) {
     nav.classList.add('active');
   } else {
