3031b5b74151827b689b1019191d54ed0602ba8c
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-12-11 14:43:11 +0000

    add animation effect

--- content-placeholder/script.js
+++ content-placeholder/script.js
@@ -1,1 +1,26 @@
-console.log('hello world');
+const header = document.getElementById('header');
+const title = document.getElementById('title');
+const excerpt = document.getElementById('excerpt');
+const profile_img = document.getElementById('profile_img');
+const name = document.getElementById('name');
+const date = document.getElementById('date');
+
+const animated_bgs = document.querySelectorAll('.animated-bg');
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text');
+
+setTimeout(getData, 2500);
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1559163499-413811fb2344?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="" />';
+  title.innerHTML = 'Lorem ipsum dolor sit amet';
+  excerpt.innerHTML =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis';
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/men/57.jpg" alt="" />';
+  name.innerHTML = 'John Doe';
+  date.innerHTML = '11 December 2020';
+
+  animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'));
+  animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'));
+}
