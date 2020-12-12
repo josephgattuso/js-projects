9657d3aba3836c342dcfb73c5139c1751b00d382
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-12-12 14:15:10 +0000

    add heart click functions

--- double-heart-click/script.js
+++ double-heart-click/script.js
@@ -1,1 +1,43 @@
-console.log('hello world');
+const loveMe = document.querySelector('.loveMe');
+const times = document.querySelector('#times');
+
+let clickTime = 0;
+let timesClicked = 0;
+
+loveMe.addEventListener('click', (e) => {
+  if (clickTime === 0) {
+    clickTime = new Date().getTime();
+  } else {
+    if (new Date().getTime() - clickTime < 800) {
+      createHeart(e);
+      clickTime = 0;
+    } else {
+      clickTime = new Date().getTime();
+    }
+  }
+});
+
+const createHeart = (e) => {
+  const heart = document.createElement('i');
+  heart.classList.add('fas');
+  heart.classList.add('fa-heart');
+
+  const x = e.clientX;
+  const y = e.clientY;
+
+  // get cooordinates within image (not the viewport)
+  const leftOffset = e.target.offsetLeft;
+  const topOffset = e.target.offsetTop;
+
+  const xInside = x - leftOffset;
+  const yInside = y - topOffset;
+
+  heart.style.top = `${yInside}px`;
+  heart.style.left = `${xInside}px`;
+
+  loveMe.appendChild(heart);
+
+  times.innerHTML = ++timesClicked;
+
+  setTimeout(() => heart.remove(), 1000);
+};
