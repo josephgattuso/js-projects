275028f867d45952ce5f21e301ef26ce7856b14f
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-12-12 12:48:57 +0000

    search for user data with axios

--- github-profiles/script.js
+++ github-profiles/script.js
@@ -1,1 +1,28 @@
-console.log('hello world');
+const APIURL = 'https://api.github.com/users/';
+
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+// getUser('josephgattuso');
+
+async function getUser(username) {
+  try {
+    const { data } = await axios(APIURL + username);
+
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const user = search.value;
+
+  if (user) {
+    getUser(user);
+
+    search.value = '';
+  }
+});
