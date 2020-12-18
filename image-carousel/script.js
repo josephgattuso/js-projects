089d6fd7966899b15cd57dc00e2198ec2410200c
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-12-18 13:40:03 +0000

    add carousel functions

--- image-carousel/script.js
+++ image-carousel/script.js
@@ -1,1 +1,44 @@
-console.log('hello world');
+const imgs = document.getElementById('imgs');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+const img = document.querySelectorAll('#imgs img');
+
+let idx = 0;
+
+// 2000ms = 2secs
+let interval = setInterval(run, 2000);
+
+function run() {
+  idx++;
+  changeImage();
+}
+
+function changeImage() {
+  if (idx > img.length - 1) {
+    idx = 0;
+  } else if (idx < 0) {
+    idx = img.length - 1;
+  }
+
+  // carousel animation
+  imgs.style.transform = `translateX(${-idx * 500}px)`;
+}
+
+function resetInterval() {
+  clearInterval(interval);
+  interval = setInterval(run, 2000);
+}
+
+rightBtn.addEventListener('click', () => {
+  idx++;
+
+  changeImage();
+  resetInterval();
+});
+
+leftBtn.addEventListener('click', () => {
+  idx--;
+
+  changeImage();
+  resetInterval();
+});
