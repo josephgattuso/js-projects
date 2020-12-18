22e80870d397f28b499d36fe4dfc262ab11b1526
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-12-18 18:50:39 +0000

    fetch pokemon data

--- pokedex/script.js
+++ pokedex/script.js
@@ -1,1 +1,35 @@
-console.log('hello world');
+const poke_container = document.getElementById('poke-container');
+const pokemon_count = 150;
+const colors = {
+  fire: '#fddfdf',
+  grass: '#defde0',
+  electric: '#fcf7de',
+  water: '#def3fd',
+  ground: '#f4e7da',
+  rock: '#d5d5d4',
+  fairy: '#fceaff',
+  poison: '#98d7a5',
+  bug: '#f8d5a3',
+  dragon: '#97b3e6',
+  psychic: '#eaeda1',
+  flying: '#f5f5f5',
+  fighting: '#e6e0d4',
+  normal: '#f5f5f5',
+};
+
+const main_types = Object.keys(colors);
+
+const fetchPokemons = async () => {
+  for (let i = 1; i <= pokemon_count; i++) {
+    await getPokemon(i);
+  }
+};
+
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data);
+};
+
+fetchPokemons();
